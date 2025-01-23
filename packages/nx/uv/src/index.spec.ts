fdf6772762196f5fda877613a3650b48e36c91db
Andrew Young <joven@alum.wpi.edu>
2025-01-23 11:53:09 -0500

    Added minimal test to uv plugin

---
+++ packages/nx/uv/src/index.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from "vitest"
+
+describe("UV Package", () => {
+  it("should pass a basic test", () => {
+    expect(true).toBe(true)
+  })
+}) 
--- packages/nx/uv/vite.config.ts
+++ packages/nx/uv/vite.config.ts
@@ -1,11 +1,43 @@
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
 export default defineConfig({
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    outDir: '../../../dist/packages/nx/uv',
+    emptyOutDir: true,
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'uv',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es'],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: [],
+    },
+  },
   root: __dirname,
   cacheDir: '../../../node_modules/.vite/uv',
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [
+    nxViteTsPaths(),
+    nxCopyAssetsPlugin(['*.md']),
+    dts({
+      entryRoot: 'src',
+      tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
+    }),
+  ],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
